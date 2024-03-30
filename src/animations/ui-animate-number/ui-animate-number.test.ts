2a45a7125b0bc083a4bb19fe7292e7378b5fee0f
varodv <varodv@gmail.com>
2024-03-30 22:38:24 +0100

    test: Improve unit tests

--- src/animations/ui-animate-number/ui-animate-number.test.ts
+++ src/animations/ui-animate-number/ui-animate-number.test.ts
@@ -1,9 +1,25 @@
+import { LitElement } from 'lit';
 import './';
 
+async function setupTest({
+  content,
+}: {
+  content: string;
+}): Promise<{ element: LitElement }> {
+  const element = document.createElement('ui-animate-number') as LitElement;
+  element.innerHTML = content;
+  document.body.appendChild(element);
+  await element.updateComplete;
+
+  return {
+    element,
+  };
+}
+
 describe('UiAnimateNumber', () => {
-  test('should render the content specified on its slot', () => {
+  test('should render the content passed as slot', async () => {
     const content = '1,000,000.00';
-    document.body.innerHTML = `<ui-animate-number>${content}</ui-animate-number>`;
+    await setupTest({ content });
 
     expect(document.body.innerHTML).toContain(content);
   });
